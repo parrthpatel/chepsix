8de97b05c2a65559fb44152fff68f60d3713b516
parth1634 <parth1634@gmail.com>
2015-12-24 13:31:23 +0530

    gem file modifed and user added

--- Gemfile
+++ Gemfile
@@ -9,6 +9,10 @@ gem 'coffee-rails', '4.1.0'
 gem 'jquery-rails', '4.0.3'
 gem 'turbolinks',   '2.3.0'
 gem 'jbuilder',     '2.2.3'
+gem 'faker',                '1.4.2'
+gem 'will_paginate',           '3.0.7'
+gem 'bootstrap-will_paginate', '0.0.10'
+
 gem 'sdoc',         '0.4.0', group: :doc
 
 group :development, :test do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,17 @@
+User.create!(name:  "example man",
+             email: "example@exampleoff.com",
+             password:              "foobar",
+             password_confirmation: "foobar")
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@exampleoff.com"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
