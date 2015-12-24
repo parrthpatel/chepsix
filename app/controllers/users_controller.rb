68e4d72304e7e1b1d0cffc811151fe010d4a54e8
parth1634 <parth1634@gmail.com>
2015-12-24 14:47:47 +0530

    User Delete function and admin user created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
 
   def show
     @user = User.find(params[:id])
@@ -13,6 +15,11 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+  end
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
 
   def create
@@ -61,5 +68,8 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
