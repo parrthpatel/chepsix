018a16226885475311200bf59ef00bf74b7af95e
parth1634 <parth1634@gmail.com>
2015-12-29 14:11:35 +0530

    Add Acount Activation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 User.create!(name:  "example man",
              email: "example@exampleoff.com",
              password:              "foobar",
-             password_confirmation: "foobar")
+             password_confirmation: "foobar",
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
@@ -10,7 +12,9 @@ User.create!(name:  "example man",
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
