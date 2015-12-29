9284397c0c5cf1c0541b07fbf5ff990041f9c1da
parth1634 <parth1634@gmail.com>
2015-12-29 18:47:50 +0530

    Athentication done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	 attr_accessor :remember_token, :activation_token
+	 attr_accessor :remember_token, :activation_token, :reset_token
 	 before_save   :downcase_email
   	 before_create :create_activation_digest
 
@@ -25,31 +25,52 @@ class User < ActiveRecord::Base
 
 	 end
 	
-	def authenticated?(attribute, token)
+	 def authenticated?(attribute, token)
 	    digest = send("#{attribute}_digest")
 	    return false if digest.nil?
 	    BCrypt::Password.new(digest).is_password?(token)
-    end
+     end
 	
-	def forget
-	    update_attribute(:remember_digest, nil)
-  	end
-  	def activate
-      update_attribute(:activated,    true)
-      update_attribute(:activated_at, Time.zone.now)
-    end
+	 def forget
+	     update_attribute(:remember_digest, nil)
+  	 end
+# Activates an account.
+	 def activate
+	   update_attribute(:activated,    true)
+	   update_attribute(:activated_at, Time.zone.now)
+	 end
 
-    # Sends activation email.
-    def send_activation_email
-      UserMailer.account_activation(self).deliver_now
-    end
+	  # Sends activation email.
+	 def send_activation_email
+	   UserMailer.account_activation(self).deliver_now
+	 end
 
-  	 private
-  	 	def downcase_email
-      		self.email = email.downcase
-    	end
-    	def create_activation_digest
-	      self.activation_token  = User.new_token
-	      self.activation_digest = User.digest(activation_token)
-    	end
+	# Sets the password reset attributes.
+   	 def create_reset_digest
+	   self.reset_token = User.new_token
+	   update_attribute(:reset_digest,  User.digest(reset_token))
+	   update_attribute(:reset_sent_at, Time.zone.now)
+	 end
+  # Sends password reset email.
+	 def send_password_reset_email
+	   UserMailer.password_reset(self).deliver_now
+	 end
+
+	 # Returns true if a password reset has expired.
+     def password_reset_expired?
+       reset_sent_at < 2.hours.ago
+     end
+
+	 private
+
+	  # Converts email to all lower-case.
+	   def downcase_email
+	     self.email = email.downcase
+	   end
+
+	    # Creates and assigns the activation token and digest.
+	   def create_activation_digest
+	     self.activation_token  = User.new_token
+	     self.activation_digest = User.digest(activation_token)
+	   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'account_activations/edit'
 
   get 'sessions/new'
@@ -15,6 +19,7 @@ Rails.application.routes.draw do
   delete 'logout' => 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
