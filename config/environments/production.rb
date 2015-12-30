f3bd64f1879de2d34fc11fc3b532fd707723622d
parth1634 <parth1634@gmail.com>
2015-12-30 16:54:05 +0530

    change done in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   config.force_ssl = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'glacial-taiga-6415.herokuapp.com'
+  host = 'glacial-taiga-6415'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
