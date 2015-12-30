023b8c27e7d893ffe6155dad2cf4ad91a0cc38c8
parth1634 <parth1634@gmail.com>
2015-12-30 12:37:59 +0530

    change completed in email registration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,14 +79,14 @@ Rails.application.configure do
   config.force_ssl = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'glacial-taiga-6415.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['parth1634'],
-    :password       => ENV['dhruvdhruv2'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
